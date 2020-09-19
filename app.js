5cb3afc3f69bbf17d8ccab1a82f2d66a84e16aee
tberto <tberto@br.ibm.com>
2020-09-18 23:17:42 -0300

    fixing error promise

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const github = require('octonode');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors())
 
 app.get('/api/repositories', async (req, res) => {
     let repoName = req.query.name;
@@ -26,21 +29,30 @@ async function listRepo(client, name = '') {
     return new Promise((resolve, reject) => {
         let ghme = client.me();
         ghme.info((err, body) => {
-            if (err) throw 500;
-            let login = body.login;
-            let queryParam = 'user:' + login + " " + (name != ''? name +' in:name': '');
-            let query = {q: queryParam};
-            client.search().repos(query, function(err, body, header) {
-                if (err) throw 500;
-                let repos = body.items.map(function(repo){
-                    return {
-                        'name' : repo.name,
-                        'url' : repo.url,
-                        'language': repo.language
+            if (err){
+                reject(500);
+            } else {
+                let login = body.login;
+                let queryParam = 'user:' + login + " " + (name != ''? name +' in:name': '');
+                let query = {q: queryParam};
+                client.search().repos(query, function(err, body, header) {
+                    if (err){
+                        reject(500);
+                    } else {
+                        let repos = body.items.map(function(repo){
+                            return {
+                                'name' : repo.name,
+                                'url' : repo.html_url,
+                                'language': repo.language
+                            }
+                        });
+                        resolve({'repositories': repos});
                     }
+                   
                 });
-                resolve({'repositories': repos});
-            });
+            }
+            
+            
         })
     })
 }
